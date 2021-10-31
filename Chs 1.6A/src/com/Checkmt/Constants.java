a2b67c0ce065b4b986d4adcdcdb28ce12d31564b
elibenyair <elibenyair@gmail.com>
2021-10-31 23:05:42 +0200

    commit test 3

--- Chs 1.6A/src/com/Checkmt/Constants.java
+++ Chs 1.6A/src/com/Checkmt/Constants.java
@@ -5,3 +5,4 @@ public class Constants {
 	public static final boolean BLACK = false;
 
 }
+// ----------------
--- Chs 1.6A/src/com/Checkmt/Coords.java
+++ Chs 1.6A/src/com/Checkmt/Coords.java
@@ -15,3 +15,4 @@ public class Coords implements Serializable {
 
 }
 // ------------
+// ---------2--
